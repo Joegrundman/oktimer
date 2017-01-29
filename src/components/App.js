ae8a32bc2e92b981b14a330ee8a18ed8ad2b22d1
Joe Grundman <Joe Grundman>
2017-01-29 01:34:57 +0000

    ok timer connected

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Timer from './Timer'
+import debounce from '../debounce'
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props)
-
-
     this.state = {
       timers: ['eat', 'drink', 'fuck']
     }
-
     this.handleClose = this.handleClose.bind(this)
   }
 
@@ -19,13 +17,24 @@ class App extends Component {
       const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
       this.recognition = new SpeechRecognition()
       this.recognition.interimResults = true
-      this.recognition.addEventListener('result', e => {
-        console.log(e)
-      })
+      this.recognition.addEventListener('result', debounce(this.detectOkTimer, 200, true))
       this.recognition.addEventListener('end', this.recognition.start)
       this.recognition.start()
   }
-  
+ 
+  detectOkTimer(e) {
+    const transcript = Array.from(e.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+
+      if(transcript.toLowerCase() === ("ok timer")) {
+          const msg = new SpeechSynthesisUtterance()
+          msg.text = "OK"
+          speechSynthesis.speak(msg)
+        }
+  }
+
   handleClose (target) {
     const timers = this.state.timers.filter(t => timers !== t)
     this.setState({
