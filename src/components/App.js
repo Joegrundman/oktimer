29854061511ba88007646bbd366acdd47fd9b0f7
Joe Grundman <Joe Grundman>
2017-01-29 11:00:31 +0000

    added times

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Timer from './Timer'
-import debounce from '../debounce'
+import { isTimer, parseTimer } from '../speechtimes'
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props)
+
+    this.STATUS = {
+      STANDBY : 'STATUS_STANDBY',
+      LISTENING: 'LISTENING'
+    }
+
+    const defaultTimers = ['eat', 'drink', 'fuck', 'shag sheep', 'drink schnapps', 'talk about submarines']
+    const defaultTimersReady = []
+    defaultTimers.forEach((defaultTimer, i) => {
+        defaultTimersReady.push({
+          name: defaultTimer,
+          timer: setTimeout(() => this.timeIsUp(defaultTimer), 10000 * (i + 1))
+        })
+    })
+
     this.state = {
-      timers: ['eat', 'drink', 'fuck']
+      timers: defaultTimersReady,
+      status: this.STATUS.STANDBY
     }
     this.handleClose = this.handleClose.bind(this)
   }
@@ -17,22 +33,44 @@ class App extends Component {
       const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
       this.recognition = new SpeechRecognition()
       this.recognition.interimResults = true
-      this.recognition.addEventListener('result', debounce(this.detectOkTimer, 200, true))
+      this.recognition.addEventListener('result', this.handleSpeechEvent)
       this.recognition.addEventListener('end', this.recognition.start)
       this.recognition.start()
   }
- 
+
+  handleSpeechEvent(e) {
+     if (this.state.status === this.STATUS.STANDBY) {
+       this.detectOkTimer(e)
+     } 
+     else if(this.state.status === this.STATUS.LISTENING){
+       this.parseMessage(e)
+     }
+  }
+
   detectOkTimer(e) {
     const transcript = Array.from(e.results)
           .map(result => result[0])
           .map(result => result.transcript)
           .join('')
 
-      if(transcript.toLowerCase() === ("ok timer")) {
+      if(e.results[0].isFinal && transcript.toLowerCase() === ("ok timer")) {
           const msg = new SpeechSynthesisUtterance()
           msg.text = "OK"
           speechSynthesis.speak(msg)
         }
+  }
+
+  parseMessage(e) {
+    const transcript = Array.from(e.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+
+      if(e.results[0].isFinal) {
+        if(isTimer(transcript)) {
+
+        }
+      }
   }
 
   handleClose (target) {
@@ -42,19 +80,27 @@ class App extends Component {
     })
   }
 
+  timeIsUp(name) {
+    console.log('timer has expired:',name)
+    const nextTimers = this.state.timers.filter(t => t.name !== name)
+    this.setState({
+      timers: nextTimers
+    })
+  }
+
   render() {
 
     const timers = this.state.timers.map((t,i) => (
-      <Timer key={i} onClose={this.handleClose}>{t}</Timer>
+      <Timer key={i} onClose={this.handleClose}>{t.name}</Timer>
       ))
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>OK Timer</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To start a new timer, say "OK Timer", then say the message and the time.
         </p>
         {timers}
       </div>
