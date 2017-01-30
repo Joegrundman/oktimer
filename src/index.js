59fac1605a7c6cc00dc5b668b76cf6dc59c4e5a9
Joe Grundman <Joe Grundman>
2017-01-30 17:47:18 +0000

    add material-ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './components/App';
 import './index.css';
 
+//Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+const OkTimer = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+
 ReactDOM.render(
-  <App />,
+  <OkTimer />,
   document.getElementById('root')
 );
