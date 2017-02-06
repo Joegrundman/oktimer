067c71d6cdff18e8a983fdd5140f4f4f02a991b0
Joe Grundman <Joe Grundman>
2017-02-06 14:30:35 +0000

    colors to show timer expired on timer panel

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,30 +1,36 @@
 import React, { Component, PropTypes } from 'react'
 import Paper from 'material-ui/Paper'
-import FlatButton from 'material-ui/FlatButton';
+import FlatButton from 'material-ui/FlatButton'
+import { amber100 } from 'material-ui/styles/colors'
 
 class Timer extends Component {
 
     static propTypes = {
-        name: PropTypes.string.isRequired,
-        timeMsg: PropTypes.string.isRequired,
+        timer: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            timeMsg: PropTypes.string.isRequired, 
+            expired: PropTypes.bool          
+        }),
         cancelTimer: PropTypes.func.isRequired
     }
 
     render(){
 
-        const style = {
+        let style = {
             paddingTop: '10px',
             paddingBottom: '10px',
             width: '80%',
             margin: '0 auto',
-            marginTop: '10px'
+            marginTop: '10px',
+            backgroundColor: this.props.timer.expired ? amber100 : '#fff'
+            
         }
 
         return (
             <Paper style={style}>
-                <span>{this.props.name} - {this.props.timeMsg}</span> 
+                <span>{this.props.timer.name} - {this.props.timer.timeMsg}</span> 
                 <FlatButton
-                    label="cancel"
+                    label={this.props.timer.expored ? "dismiss" :"cancel"}
                     primary={true}
                     onClick={this.props.cancelTimer}/>
             </Paper>
--- src/modules/speech.js
+++ src/modules/speech.js
@@ -90,3 +90,5 @@ export function speak (text, voice, callback) {
 }
 
 
+
+
